0e70f456dde6ad9c52c855d345a0a1427252e214
Humberto Sandmann <hsandmann@ieee.org>
2024-03-13 12:47:42 -0300

    new version

--- src/main/java/insper/store/auth/AuthService.java
+++ src/main/java/insper/store/auth/AuthService.java
@@ -18,6 +18,7 @@ public class AuthService {
     @Autowired
     private JwtService jwtService;
 
+    @SuppressWarnings("null")
     public String register(Register in) {
         final String password = in.password().trim();
         if (null == password || password.isEmpty()) throw new IllegalArgumentException("Password is required");
@@ -45,6 +46,7 @@ public class AuthService {
         final AccountOut account = response.getBody();
 
         // Cria um token JWT
+        @SuppressWarnings("null")
         final String token = jwtService.create(account.id(), account.name(), "regular");
 
         return LoginOut.builder()
